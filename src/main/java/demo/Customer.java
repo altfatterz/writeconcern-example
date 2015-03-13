79253efa5009d07e8081c809854fe9dcb26a9d31
Zoltan Altfatter <altfatterz@gmail.com>
2015-03-13 18:17:26 +0100

    add mapping a Map into mongo

--- src/main/java/demo/Customer.java
+++ src/main/java/demo/Customer.java
@@ -4,6 +4,10 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 @Document(collection = "customers")
 public class Customer {
 
@@ -14,6 +18,8 @@ public class Customer {
 
     @DBRef
     private Address address;
+
+    private Map<String, House> houses = new HashMap<>();
 
     public String getName() {
         return name;
@@ -34,4 +40,16 @@ public class Customer {
     public String getId() {
         return id;
     }
+
+    public void addHouse(House house) {
+        String houseId = UUID.randomUUID().toString();
+        house.setId(houseId);
+
+        houses.put(houseId, house);
+    }
+
+    public Map<String, House> getHouses() {
+        return houses;
+    }
+
 }
--- src/main/java/demo/DataLoader.java
+++ src/main/java/demo/DataLoader.java
@@ -1,11 +1,11 @@
 package demo;
 
-import com.mongodb.WriteConcern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.util.Map;
 
 @Component
 public class DataLoader {
@@ -24,8 +24,34 @@ public class DataLoader {
         addressRepository.deleteAll();
         customerRepository.deleteAll();
 
-        // set global write concern for write operations
-        mongoTemplate.setWriteConcern(WriteConcern.JOURNALED);
+        Customer customer = new Customer();
+        customer.setName("Zoltan");
+
+        Address address = new Address();
+        address.setStreetName("Stadionlaan");
+
+        House house = new House();
+        house.setName("house1");
+        customer.addHouse(house);
+
+        house = new House();
+        house.setName("house2");
+        customer.addHouse(house);
+
+        addressRepository.save(address);
+        // here can be a problem
+        customerRepository.save(customer);
+
+        Customer one = customerRepository.findOne(customer.getId());
+
+        Map<String, House> houses = one.getHouses();
+        for (String s : houses.keySet()) {
+            System.out.println(s + " : " + houses.get(s).getName());
+        }
+
+
+        // set global write concern for write operations, default is WriteConcern.ACKNOWLEDGED
+        //mongoTemplate.setWriteConcern(WriteConcern.JOURNALED);
 
         // set write concern per operation basis
         // mongoTemplate.setWriteConcernResolver(new CustomerWriteConcernResolver());
